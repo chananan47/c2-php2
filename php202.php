b3cbd1987f17a249849bbe5c66e4258154ef0e5e
chananan47 <namayura47@gmail.com>
2021-11-12 11:32:30 +0700

    initial commit

--- php202.php
+++ php202.php
@@ -1,3 +1,22 @@
-<?php
-    echo "My Website";
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content = "device=wigth-device, initial-scale=1.0">
+        <title>PHP: Hypertext Preprocessor.</title>
+    </head>
+
+    <body>
+        <h1>Welcome to php</h1>
+        <hr />
+        <h2>
+        <?php 
+            echo "Hello World!"; 
+            echo "<br />";
+            echo "My Name is Mar sakaberg";
+            echo "<br /">;
+            print("I Love Face Book");
+        ?>
+        </h2>
+    </body>
+</html>
